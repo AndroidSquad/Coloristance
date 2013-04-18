1dc8f0d68a92f9cec4e8834f799bf779c0d7a8a6
alexandermafi <alexander.mafi@gmail.com>
2013-04-18 22:45:33 +0200

    Add music pilot_jones

--- src/se/androidsquad/coloristance/MainActivity.java
+++ src/se/androidsquad/coloristance/MainActivity.java
@@ -1,23 +1,37 @@
 package se.androidsquad.coloristance;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+
 public class MainActivity extends Activity {
+
+	MediaPlayer mp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mp = MediaPlayer.create(MainActivity.this, R.raw.pilot_jones);				    
+		mp.start();
 	}
-
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		mp.release();
+	}
+ 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
 
 }
