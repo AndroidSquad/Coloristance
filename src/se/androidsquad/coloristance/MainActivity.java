9633b24e85a6e78ea95829ac1354a3ff4976c4f1
alexandermafi <alexander.mafi@gmail.com>
2013-04-19 12:09:59 +0200

    Music plays and pauses with buttons

--- src/se/androidsquad/coloristance/MainActivity.java
+++ src/se/androidsquad/coloristance/MainActivity.java
@@ -1,9 +1,13 @@
 package se.androidsquad.coloristance;
 
+import java.io.IOException;
+
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -14,19 +18,62 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mp = MediaPlayer.create(MainActivity.this, R.raw.music);				 
 
-		mp = MediaPlayer.create(MainActivity.this, R.raw.music);				    
-		mp.start();
-		mp.setLooping(true);
+		
+		Button b = (Button) findViewById(R.id.button2);
+		b.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//Nedan behövs för att ta MediaPlayer från Initialized State till Prepared State
+				try {
+					mp.prepare();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				 
+				mp.start();
+				mp.setLooping(true);
+			}
+		});
+
+		Button c = (Button) findViewById(R.id.button3);
+		c.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// Nedan behövs för att trycka på Sexy Music off
+				// första gången innan MediaPlayer är i Started State
+				
+				try {
+					mp.prepare();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				 
+				mp.start();
+				mp.pause();	
+			}
+		});
 	}
-	
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
 		mp.release();
+
 	}
- 
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
