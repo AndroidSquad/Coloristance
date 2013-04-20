bdcf9425a61d458c7e3dfdf6b0c9e6d04afc1bdc
alexandermafi <alexander.mafi@gmail.com>
2013-04-20 20:56:54 +0200

    Created a second activity that launches when pressing New Game

---
+++ src/se/androidsquad/coloristance/FirstScreen.java
@@ -0,0 +1,14 @@
+package se.androidsquad.coloristance;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.firstscreen);
+	}	
+}
--- src/se/androidsquad/coloristance/MainActivity.java
+++ src/se/androidsquad/coloristance/MainActivity.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,17 @@ public class MainActivity extends Activity {
 		
 		mp = MediaPlayer.create(MainActivity.this, R.raw.music);				 
 
+		
+		Button a = (Button) findViewById(R.id.button1);
+		a.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this, FirstScreen.class));
+				
+			}
+		});
 		
 		Button b = (Button) findViewById(R.id.button2);
 		b.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +77,7 @@ public class MainActivity extends Activity {
 				mp.pause();	
 			}
 		});
+		
 	}
 	@Override
 	protected void onPause() {
