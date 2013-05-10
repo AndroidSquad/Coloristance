d3eba3f76adf69e5ebd117ee7270b3d8a311cd5d
askerik <askerik@student.chalmers.se>
2013-05-10 14:56:51 +0200

    added information about which number that will be represented by each number

--- src/se/androidsquad/coloristance/Levels.java
+++ src/se/androidsquad/coloristance/Levels.java
@@ -3,5 +3,11 @@ package se.androidsquad.coloristance;
 public class Levels {
  /*
   * This class is supposed to describe the different levels that is included in the game
+  * blue=1
+  * green=2
+  * yellow=3
+  * red=4
+  * 
   */
+	
 }
