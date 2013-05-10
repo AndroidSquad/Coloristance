8c6a6cd9c4c137372453177838243cc2f48ed58b
askerik <askerik@student.chalmers.se>
2013-05-10 13:46:43 +0200

    started implementing the methods needed

--- src/se/androidsquad/coloristance/PositionModel.java
+++ src/se/androidsquad/coloristance/PositionModel.java
@@ -1,9 +1,19 @@
 package se.androidsquad.coloristance;
 
+import java.lang.reflect.Array;
+
 public class PositionModel {
 	
 	/*Denna modell innehåller information om vart spelaren 
 	 * befinner sig så att kartan kan markera den aktulla rutan.
+	 * 
 	 */
-
+	Array position;
+	
+	protected void setPosition(Array[][] position){
+		
+	}
+	protected Array getPosition(){
+		return null;
+	}
 }
