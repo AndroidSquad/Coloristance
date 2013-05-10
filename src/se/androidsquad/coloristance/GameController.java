cc958428ae4adf608e40b8556d23f57680224539
Simonwe <simonwesssberg@gmail.com>
2013-05-10 17:10:33 +0200

    Inlett arbete på GameController

--- src/se/androidsquad/coloristance/GameController.java
+++ src/se/androidsquad/coloristance/GameController.java
@@ -1,22 +1,45 @@
 package se.androidsquad.coloristance;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
 public class GameController {
-	
-	/*Koordinerar mellan View och Model.Bör innehålla metoderna:
+
+	/*
+	 * Koordinerar mellan View och Model.Bör innehålla metoderna:
 	 * 
-	 * doorClick()
-	 * 	- changeRoom()
-	 * 	- changeMapLocation()
-	 * 	- animateDoor()
+	 * doorClick() - changeRoom() - changeMapLocation() - animateDoor()
 	 * 
-	 * drawMap(Grid) tar rutnät från modell
-	 * 	- here(getPosition) tar emot position från PositionModel
-	 * 	- Neighbours(Door, Connect) tar emot dörrar och de olika dörrarnas relationer till varandra
-	 * 	- Rects(color) Hämtar info från modellen om vilka färger rektanglarna ska ha i kartan
+	 * drawMap(Grid) tar rutnät från modell - here(getPosition) tar emot
+	 * position från PositionModel - Neighbours(Door, Connect) tar emot dörrar
+	 * och de olika dörrarnas relationer till varandra //ANVƒNDS EJ JUST NU -
+	 * Rects(color) Hämtar info från modellen om vilka färger rektanglarna ska
+	 * ha i kartan
 	 * 
-	 * drawRoom()
-	 * 	- drawRect(color) tar info om vilken färg rummets ruta ska målas i
-	 * 	- drawDoor(position) tar info om vart dörrarna ska placeras i rummet
+	 * drawRoom() - drawRect(color) tar info om vilken färg rummets ruta ska
+	 * målas i - drawDoor(position) tar info om vart dörrarna ska placeras i
+	 * rummet
 	 */
 
+	private PositionModel pos;
+	private Levels level = new Levels();
+	private RectModel rect;
+	private DoorModel door;
+
+	protected void drawMap() {
+		int posX, posY;
+		pos = new PositionModel();
+		posX = pos.getX(); // ska skickas til view att i denna positionen i rutn‰tet ska en cirkel ritas ut
+		posY = pos.getY();
+		
+
+	}
+
+	protected void drawRoom() {
+
+	}
+
+	protected void doorClick() {
+
+	}
 }
--- src/se/androidsquad/coloristance/Levels.java
+++ src/se/androidsquad/coloristance/Levels.java
@@ -10,10 +10,21 @@ public class Levels {
   * 5=Connecting path between two rooms, under construction.
   * 
   */
+
+int[][] Level1 = {	{0,4,3,4,0},
+					{1,2,0,1,3},
+					{0,1,3,2,0}};
+
+public int length=Level1.length;
+public int height=Level1[0].length;		
+
 	
-int[][]	Level1 = {	{0,0,4,5,3,5,4,0,0},
-					{0,0,5,0,0,0,5,0,0},
-					{1,5,2,0,0,0,1,5,3}, 
-					{0,0,5,0,0,0,5,0,0}, 
-					{0,0,1,5,3,5,2,0,0}};
-}
+int[][]	Level1ALT = {	{0,0,4,5,3,5,4,0,0},
+						{0,0,5,0,0,0,5,0,0},
+						{1,5,2,0,0,0,1,5,3}, 
+						{0,0,5,0,0,0,5,0,0}, 
+						{0,0,1,5,3,5,2,0,0}};
+
+
+		
+};
