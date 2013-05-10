7f74b255eee4a08842dceed7a1f0428cdb7051b8
Simonwe <simonwesssberg@gmail.com>
2013-05-10 15:42:41 +0200

    Added a first level

--- src/se/androidsquad/coloristance/Levels.java
+++ src/se/androidsquad/coloristance/Levels.java
@@ -2,12 +2,18 @@ package se.androidsquad.coloristance;
 
 public class Levels {
  /*
-  * This class is supposed to describe the different levels that is included in the game
-  * blue=1
-  * green=2
-  * yellow=3
-  * red=4
+  * This class is supposed to describe the different levels that are included in the game
+  * 1=Blue
+  * 2=Green
+  * 3=Yellow
+  * 4=Red
+  * 5=Connecting path between two rooms, under construction.
   * 
   */
 	
+int[][]	Level1 = {	{0,0,4,5,3,5,4,0,0},
+					{0,0,5,0,0,0,5,0,0},
+					{1,5,2,0,0,0,1,5,3}, 
+					{0,0,5,0,0,0,5,0,0}, 
+					{0,0,1,5,3,5,2,0,0}};
 }
