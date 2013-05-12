db2cb6cc43bbc57273fc26896c6288f39d95f27c
alexandermafi <alexander.mafi@gmail.com>
2013-05-12 17:15:36 +0200

    Added the input parameter DrawingRect in the GameController

--- src/se/androidsquad/coloristance/DoorModel.java
+++ src/se/androidsquad/coloristance/DoorModel.java
@@ -1,8 +1,10 @@
 package se.androidsquad.coloristance;
 
 import android.widget.Button;
+import android.app.Activity;
+import android.graphics.Paint;
 
-public class DoorModel {
+public class DoorModel extends Activity {
 
 	/* Innehåller information om färgen på dörrarna och dess placering relativt rutan. 
 	 */
@@ -17,8 +19,29 @@ public class DoorModel {
 	 * Id:t hittar ni i xml filen
 	 */
 	
-
+	Button doorTop = (Button) findViewById(R.id.top_door);
+	Button doorBot = (Button) findViewById(R.id.bot_door);
+	Button doorRight = (Button) findViewById(R.id.right_door);
+	Button doorLeft = (Button) findViewById(R.id.left_door);
 	
+	DoorModel(){System.out.println("DoorModel need an int and a String");}
+	
+	DoorModel(int col, String pos){
+		/** 
+		 * DoorModel tar emot ett paint objekt som är dörr färgen och 
+		 * en int som representerar dörrarna representeras av pos 1-4
+		 * 
+		 * Informationen används för att tillskriva rätt dörr rätt färg
+		 */
+		
+		if(pos == "1") doorTop.setBackgroundColor(col);
+		else if(pos == "2") doorTop.setBackgroundColor(col);
+		else if(pos == "3") doorTop.setBackgroundColor(col);
+		else if(pos == "4") doorTop.setBackgroundColor(col);
+		else System.out.println("Faulty string to DoorModel");
+		
+	}
+			
 	
 }
 
