49d29bce5974627d75e379d7f00ebca5a42d5fed
Anton Eksell <eksell@student.chalmers.se>
2013-05-13 20:09:35 +0200

    Started creating MapModel

--- src/se/androidsquad/coloristance/MapModel.java
+++ src/se/androidsquad/coloristance/MapModel.java
@@ -1,20 +1,38 @@
 package se.androidsquad.coloristance;
 
+import java.util.StringTokenizer;
+
 //Metoder att ha
 	//Läsa in vilken färg varje rektangel har från RectModel, dvs loopa igenom de olika strängarna och säg vilken färg
-	//
 
 public class MapModel {
 	
+	String[][] mapArray;
+	int x,y;
 
-	
-	
-	protected int getX(String roomcode){
-		return roomcode.charAt(5);
+	public MapModel(String[][] mapArray){
+		this.mapArray = mapArray;
+		renderMap();
 	}
-			
-	protected int getY(String roomcode){
-		return roomcode.charAt(6);
-		
+
+	private void renderMap(){
+		//Skall innehålla funktioner som gör instanser av alla element 
+		for(int i = 0; i<mapArray.length;i++){
+			for(int j = 0; j<mapArray[i].length;j++){
+				//mapArray[i][j].charAt(0) denna snippet är char-siffran på rummet 
+				//och skall sändas till rederaren av färgen tillsammans med positionen som är i och j 
+			}
+		}
 	}
+
+	protected void setPos(int x, int y){
+		//Dessa skall anropas vid vare tryck på en dörr
+		this.x = x;
+		this.y = y;
+	}
+	
+	protected String getPos(){
+		return mapArray[x][y];
+	}
+
 }
