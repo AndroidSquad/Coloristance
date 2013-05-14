85055ce7d0ccf541076ee1298bab518da405b214
Anton <eksell@student.chalmers.se>
2013-05-14 10:42:37 +0200

    Change in MapModel, can now get x and y

--- src/se/androidsquad/coloristance/MapModel.java
+++ src/se/androidsquad/coloristance/MapModel.java
@@ -1,11 +1,5 @@
 package se.androidsquad.coloristance;
 
-import java.io.PrintStream;
-
-import android.os.SystemClock;
-import android.util.Log;
-import android.webkit.ConsoleMessage;
-
 //Metoder att ha
 	//Läsa in vilken färg varje rektangel har från RectModel, dvs loopa igenom de olika strängarna och säg vilken färg
 
@@ -23,7 +17,7 @@ public class MapModel {
 		for(int i = 0; i<mapArray.length;i++){
 			for(int j = 0; j<mapArray[i].length;j++){
 				
-				Log.v("Pos: ", "i:"+i+" j:"+j);
+				Log.v(mapArray[i][j], "i:"+i+" j:"+j);
 				
 				//mapArray[i][j].charAt(0) denna snippet är char-siffran på rummet 
 				//och skall sändas till rederaren av färgen tillsammans med positionen som är i och j 
@@ -48,8 +42,16 @@ public class MapModel {
 		return RectModel.getRectColor();
 	}
 	
-	protected String getPos(){
+	protected String getRoom(){
 		return mapArray[x][y];
+	}
+	
+	public static int getX(){
+		return x;
+	}
+	
+	public static int getY(){
+		return y;
 	}
 
 }
