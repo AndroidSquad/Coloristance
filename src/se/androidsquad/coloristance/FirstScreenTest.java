9f50952a70722d7e7690084caba122640e9cc60e
askerik <askerik@student.chalmers.se>
2013-05-20 15:21:08 +0200

    added the first usable test class, implemented on FirstScreen

--- src/se/androidsquad/coloristance/FirstScreenTest.java
+++ src/se/androidsquad/coloristance/FirstScreenTest.java
@@ -6,11 +6,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+//diskutera licenses:)
+
+
 public class FirstScreenTest {
+
+
 
 	@Before
 	public void setUp() throws Exception {
-		MapModel.setMap("lvl_1");
+		MapModel.setMap("map_1");
+		MapModel.setPos(0, 1);
+
 	}
 
 	@After
@@ -19,10 +26,28 @@ public class FirstScreenTest {
 
 	@Test
 	public void testOnCreateBundle() {
+		
 		MapModel.moveRight();
+		assertEquals(1, MapModel.getMyX());
+		assertEquals(1,MapModel.getMyY());
+		
+		MapModel.moveUp();
 		assertEquals(1, MapModel.getMyX());
 		assertEquals(0,MapModel.getMyY());
 		
+		MapModel.moveDown();
+		assertEquals(1, MapModel.getMyX());
+		assertEquals(1,MapModel.getMyY());	
+		
+		MapModel.moveUp();
+		assertEquals(1, MapModel.getMyX());
+		assertEquals(0,MapModel.getMyY());
+		
+		MapModel.moveRight();
+		assertEquals(2, MapModel.getMyX());
+		assertEquals(0,MapModel.getMyY());
+		
 	}
+
 
 }
