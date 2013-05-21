a44082da3808c526e9b223f29232ddfd6db425f6
Anton Eksell <eksell@student.chalmers.se>
2013-05-21 23:26:48 +0200

    Got different keys in different rooms, seem to follow the keyMap but not tested

---
+++ src/se/androidsquad/coloristance/GameControllerTest.java
@@ -0,0 +1,11 @@
+package se.androidsquad.coloristance;
+
+import junit.framework.TestCase;
+
+public class GameControllerTest extends TestCase {
+
+	public void testGameController() {
+		fail("Not yet implemented");
+	}
+
+}
