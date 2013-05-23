712ccc62f40b1fdda2fa3727d06479e0f3d61898
Anton <eksell@student.chalmers.se>
2013-05-23 19:02:19 +0200

    Got rid of Colorizationbar whilst its just a waste right now

--- src/se/androidsquad/coloristance/DrawKeys.java
+++ src/se/androidsquad/coloristance/DrawKeys.java
@@ -16,7 +16,7 @@ import android.view.View;
  * This class is in charge of the painting of the current map on the screen, and also 
  * the player position.
  */
-@SuppressLint("NewApi")
+
 public class DrawKeys extends View { 
 	
 	int[] keyNames = {R.id.key_button_blue, R.id.key_button_green, R.id.key_button_orange, R.id.key_button_purple, R.id.key_button_red, 9};	
@@ -25,20 +25,20 @@ public class DrawKeys extends View {
 		super(context, attrs);
 	}
 
-	@SuppressLint("DrawAllocation")
-	@Override
-	public void onDraw(Canvas canvas) { 
-		super.onDraw(canvas);
-		
-		int mapHeight = findViewById(R.id.room_layout).getHeight();
-		int mapWidth = findViewById(R.id.room_layout).getWidth();
-		Log.v("DrawKeys","Höjd: "+mapHeight +" Bredd: "+ mapWidth);
-		
-		for(int i = 0; i<5;i++){
-			findViewById(keyNames[i]).setScaleX((mapWidth/10)/mapWidth);
-			findViewById(keyNames[i]).setScaleY((mapHeight/10)/mapHeight);
-		}
-		
-	}
+//	@SuppressLint("DrawAllocation")
+//	@Override
+//	public void onDraw(Canvas canvas) { 
+//		super.onDraw(canvas);
+//		
+//		int mapHeight = findViewById(R.id.room_layout).getHeight();
+//		int mapWidth = findViewById(R.id.room_layout).getWidth();
+//		Log.v("DrawKeys","Höjd: "+mapHeight +" Bredd: "+ mapWidth);
+//		
+//		for(int i = 0; i<5;i++){
+//			findViewById(keyNames[i]).setScaleX((mapWidth/10)/mapWidth);
+//			findViewById(keyNames[i]).setScaleY((mapHeight/10)/mapHeight);
+//		}
+//		
+//	}
 }
 
