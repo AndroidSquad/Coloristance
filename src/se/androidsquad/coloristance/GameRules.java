2a68ab3bc6b6ffc9ff39439168eb9392a3e32e30
Simonwe <simonwesssberg@gmail.com>
2013-05-28 14:18:13 +0200

    Added comments, and fixed the functionality of the resume button in the main menu

--- src/se/androidsquad/coloristance/GameRules.java
+++ src/se/androidsquad/coloristance/GameRules.java
@@ -4,24 +4,18 @@ import se.androidsquad.coloristance.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-
+/**
+ * This class is an activity that initiates the gamerules.xml which can be accessed from the main screen
+ */
 public class GameRules extends Activity{
 
+	
+	/**
+	 * When the class is called the onCreate sets the view to gamerules.xml
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.gamerules);
 	}
-
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-	}
-	
-	
-	
-	
-
 }
