960f356555cfa49508a2b164f0072fc472570bb5
Anton <eksell@student.chalmers.se>
2013-05-29 12:38:57 +0200

    Added RoomView

---
+++ src/se/androidsquad/coloristance/views/RoomView.java
@@ -0,0 +1,28 @@
+package se.androidsquad.coloristance.views;
+
+import se.androidsquad.coloristance.R;
+import se.androidsquad.coloristance.models.DoorModel;
+import se.androidsquad.coloristance.models.MapModel;
+import se.androidsquad.coloristance.models.RectModel;
+import android.app.Activity;
+import android.view.View;
+
+public class RoomView{
+
+	View currentView;
+	public Activity act;
+
+	public RoomView(Activity current) {
+		this.act = current;
+	}
+
+	/**
+	 * This method controls that DoorModel appoints the correct doors to each room
+	 * This method controls that the room is set to its corresponding color. 
+	 */
+	public void setRoom(){
+		DoorModel.setDoor(MapModel.getRoom());
+		RectModel.setRectColor(MapModel.getRoom());
+		act.findViewById(R.id.room).setBackgroundColor(RectModel.getRectColor());
+	}//setRoom
+}//RoomView
