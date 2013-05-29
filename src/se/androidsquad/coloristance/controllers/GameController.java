37b71ad484143a808ce8d98bf4e58ee2ce566360
askerik <askerik@student.chalmers.se>
2013-05-29 12:04:40 +0200

    cleaned up the code in GameController

--- src/se/androidsquad/coloristance/controllers/GameController.java
+++ src/se/androidsquad/coloristance/controllers/GameController.java
@@ -9,30 +9,6 @@ import android.util.Log;
 
 public class GameController {
 
-	/*
-	 * Koordinerar mellan View och Model.Should contain methods:
-	 * 
-	 * doorClick() - changeRoom() - changeMapLocation() - animateDoor()
-	 * 
-	 * drawMap(Grid) tar rutnŠt frŒn modell - here(getPosition) tar emot
-	 * position frŒn PositionModel - Neighbours(Door, Connect) tar emot dšrrar
-	 * och de olika dšrrarnas relationer till varandra //ANVÄNDS EJ JUST NU -
-	 * Rects(color) HŠmtar info frŒn modellen om vilka fŠrger rektanglarna ska
-	 * ha i kartan
-	 * 
-	 * drawRoom() - drawRect(color) tar info om vilken fŠrg rummets ruta ska
-	 * mŒlas i - drawDoor(position) tar info om vart dšrrarna ska placeras i
-	 * rummet
-	 */
-
-	/** För att veta vad funktionen gör och framför allt vad den tar 
-	 * emot/skickar tillbaka är dessa kommentarer utmärkta verktyg!
-	 * 
-	 *  Annat tips är: Ctr+Space när man inte vet vad som finns att välja på
-	 *  kommandot plockar upp alternativa sätt att avsluta det du skrivit. 
-	 *  Pröva ex. att i en xml-fil skriva android: och sedan trycka Ctrl+Space
-	 *  
-	 *  Om du får ett fel kan du trycka på det och därefter Ctrl+1 för förlag på lösningar*/
 
 	protected RectModel rect;
 	protected DoorModel door;
@@ -42,34 +18,23 @@ public class GameController {
 	public static KeyModel[][] key;
 	public static InventoryModel inv = new InventoryModel();
 
-	// The first level is always "map_1", why the variable level is initally defined as "map_1"
+	// The first level is always "map_1", why the variable level is initally defined 1
 	public static int level = 1; 
 
 	/*
 	 * The empty constructor of GameController whcih creates an object of 
-	 * each of the different models created
+	 * each of the three models, RectModel, DoorModel and Roomcode.
 	 */
 
-	public GameController(){ // Creates an object of each of the Models
+	public GameController(){ 
 			MapModel.setMap(level);
 			key = KeyModel.getKeyArray();
-			
-	
-//			Log.v("GameController","Fel"+level);
-//		for(int i = 0; i<key.length;i++){
-//			for(int j = 0; j<key[i].length;j++){
-//				Log.v("GameController",i +","+ j +":"+ key[i][j].getKeyString());
-//			}
-//		}
-
+		
 		this.rect = new RectModel();
 		this.door = new DoorModel();
 
-//		doorClick();
-//		Levels.initLevel();
-
-	}//Constructor
-	
+	}
+	//investigates which level you are on ,  initially the level is set to 1
 	public static void setLevel(int lvl){
 		if(1 <= lvl)
 			level = lvl;
@@ -78,42 +43,11 @@ public class GameController {
 
 	}
 	
+	/**
+	 * @return The level that you currently are playing
+	 */
 	public static int getLevel(){
 		Log.v("GameController","The level returned was: "+level);
 		return level;	
-	}
-	
-	
-	
-	/* Do we really need this constructor?
-	 * 
-	 * public GameController(PositionModel pos, Levels level, RectModel rect, DoorModel door) {
-		this.pos = pos;
-		this.level = level;
-		this.rect = rect;
-		this.door = door;
-	} */
-
-//	protected void drawMap() {
-//		int posX, posY;
-//		pos = new PositionModel();
-//		posX = pos.getX(); // ska skickas til view att i denna positionen i rutnätet ska en cirkel ritas ut
-//		posY = pos.getY();
-//
-//	}
-
-//	protected void drawRoom() {
-//		
-//	}
-//	 this model is only called once and not as it supposed more often as I understand it.............................................................
-//	protected void doorClick() {
-//		roomcode = Levels.mapArray[0][0]; //retrieving the room from the database "Levels"
-//		RectModel.setRectColor(roomcode);
-//		DoorModel.setDoor(roomcode);
-//	}
-
-	
-	
-
-	
-}//GameController
+	}	
+}
